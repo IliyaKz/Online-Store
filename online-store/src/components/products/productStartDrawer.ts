6092713048c05ee31733877b85c22c0195ae2fb3
IliyaKz <iliya-kuznetsov@mail.ru>
2022-07-14 19:39:13 +0300

    feat: add new class for drawing products

---
+++ online-store/src/components/products/productStartDrawer.ts
@@ -0,0 +1,24 @@
+import { ProductCreator } from './productCreator';
+import { Product } from './product';
+import { Sorting } from '../settings/sorting';
+import { TemplateKeeper } from '../products/productTemplate';
+
+class ProductStartDrawer {
+  creator: ProductCreator;
+
+  sorting: Sorting;
+
+  constructor() {
+    this.creator = new ProductCreator;
+    this.sorting = new Sorting;
+  }
+
+  drawProducts(): void {
+    let result: Array<Product> = this.creator.filterProducts(TemplateKeeper.currentTemplate, ProductCreator.productArray);
+    result = this.sorting.sortingProducts(result);
+    this.creator.drawProducts(result);
+  }
+
+}
+
+export { ProductStartDrawer };
